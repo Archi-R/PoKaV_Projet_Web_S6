100806a60720d05ba526a8ca89102fd32146ff51
Ronan PEYREL <archib71@gmail.com>
2023-05-24 13:53:59 +0200

    création carte

---
+++ client/cartes/script_carte.js
@@ -0,0 +1,32 @@
+// Remplissez ici les détails de votre jeu.
+// Ceci est un exemple de comment vous pourriez remplir dynamiquement les informations de votre carte en utilisant JavaScript.
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetch('http://localhost:3000/get-card')
+        .then(response => response.json())
+        .then(cardInfo => {
+            // Remplir le titre
+            document.getElementById('title').innerText = cardInfo.title;
+
+            // Remplir le type de jeu
+            document.getElementById('game-type').innerText = cardInfo.gameType;
+
+            // Remplir la liste de jetons
+            let chipListElement = document.getElementById('chip-list');
+            chipListElement.innerHTML = ''; // Vider la liste existante
+            cardInfo.chipList.forEach(chip => {
+                let listItem = document.createElement('li');
+                listItem.innerText = `${chip.color} ${chip.count} (${chip.value}$)`;
+                chipListElement.appendChild(listItem);
+            });
+
+            // Remplir le nombre total de jetons
+            document.getElementById('total-chips').innerText = `Total Chips: ${cardInfo.totalChips}`;
+
+            // Remplir le nombre de joueurs
+            document.getElementById('player-count').innerText = `Player Count: ${cardInfo.playerCount}`;
+
+            // Remplir la durée du jeu
+            document.getElementById('game-length').innerText = `Game Length: ${cardInfo.gameLength}`;
+        })
+        .catch(error => console.error('Error:', error));
+});
--- server/app.js
+++ server/app.js
@@ -1,2 +1,37 @@
 //fichier du lancement du serveur
 //importation des modules
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let cardInfo = {
+    title: 'Nom de la composition',
+    gameType: 'holdEm',
+    chipList: [
+        {color: 'rouge', count: 10, value: 10},
+        {color: 'bleu', count: 20, value: 20},
+        // Ajoutez plus d'objets pour chaque couleur de jeton
+    ],
+    totalChips: 100,
+    playerCount: 5,
+    gameLength: 'courte',
+};
+
+app.use(express.json());
+
+app.post('/update-card', (req, res) => {
+    const { title, gameType, chipList, totalChips, playerCount, gameLength } = req.body;
+
+    cardInfo.title = title;
+    cardInfo.gameType = gameType;
+    cardInfo.chipList = chipList;
+    cardInfo.totalChips = totalChips;
+    cardInfo.playerCount = playerCount;
+    cardInfo.gameLength = gameLength;
+
+    res.send('Card information updated successfully');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
