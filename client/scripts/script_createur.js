4252684ad8258e8daabf08a4f7302ad99cee925c
DryloXx2 <nicolas.e2002568@etud.univ-ubs.fr>
2023-05-29 15:27:07 +0200

    Maj front jetons

--- client/scripts/script_createur.js
+++ client/scripts/script_createur.js
@@ -3,7 +3,28 @@ const bouton = document.querySelector('button[type="submit"]');
 const spanSuccess = document.querySelector('.success');
 
 // Ajouter un gestionnaire d'événement au clic sur le bouton
-bouton.addEventListener('click', function() {
+bouton.addEventListener('click', (event) => {
+  // Empêcher le comportement par défaut du bouton
+  event.preventDefault();
+  // Récupérer les valeurs des champs de formulaire
+  // On récupère le nom de la composition dans un champ input
+  const title = document.querySelector('input[name="nom_cave"]').value;
+  // On récupère le type de jeu dans un champ select
+  const gameType = document.querySelector('select[name="type_jeu"]').value;
+  // On récupère le nombre de joueurs dans un champ input
+  const playerCount = document.querySelector('input[name="nb_joueurs"]').value;
+  // On récupère la durée de la partie dans un champ select
+  const gameLength = document.querySelector('select[name="game_length"]').value;
+
+  console.log(title, gameType, playerCount, gameLength);
+
+  // On récupère les valeurs des champs de jetons
+  // On récupère les champs de jetons dans un tableau
+  
+
+
+
+
   // Afficher la span avec la classe "success"
   spanSuccess.style.display = 'flex';
 });
