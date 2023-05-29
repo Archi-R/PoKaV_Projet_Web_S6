49a38244a672de72ee53d8f147c95837dd2e341c
Ronan PEYREL <archib71@gmail.com>
2023-05-29 16:10:37 +0200

    construction des cartes avec icon

--- client/scripts/script_classiques.js
+++ client/scripts/script_classiques.js
@@ -16,31 +16,24 @@ function createCard(cardData) {
     let sidesSections = document.createElement('div');
     let leftSection = document.createElement('div');
     let rightSection = document.createElement('div');
-    let chipList = document.createElement('ul');
-
-    cardData.chipList.forEach(chip => {
-        let listItem = document.createElement('li');
-        listItem.textContent = `${chip.color} ${chip.count} (${chip.value}$)`;
-        chipList.appendChild(listItem);
-    });
-
+    let chipsDiv = chipToDiv(cardData.chipList);
     let totalChips = document.createElement('p');
     let playerCount = document.createElement('p');
     let gameLength = document.createElement('p');
 
     // Attribuer les classes et id
     card.setAttribute('class', 'card');
-    title.setAttribute('id', 'title_card');
+    title.setAttribute('class', 'title_card');
     infoBox.setAttribute('class', 'info-box');
     typePartie.setAttribute('class', 'typepartie');
-    gameType.setAttribute('id', 'game-type');
+    gameType.setAttribute('class', 'game-type');
     sidesSections.setAttribute('class', 'sidessections');
     leftSection.setAttribute('class', 'left-section');
     rightSection.setAttribute('class', 'right-section');
-    chipList.setAttribute('id', 'chip-list');
-    totalChips.setAttribute('id', 'total-chips');
-    playerCount.setAttribute('id', 'player-count');
-    gameLength.setAttribute('id', 'game-length');
+    chipsDiv.setAttribute('class', 'chip-list');
+    totalChips.setAttribute('class', 'total-chips');
+    playerCount.setAttribute('class', 'player-count');
+    gameLength.setAttribute('class', 'game-length');
 
     // Ajouter les données de la carte aux éléments créés
     title.textContent = cardData.title;
@@ -51,7 +44,7 @@ function createCard(cardData) {
 
     // Assembler les éléments
     typePartie.appendChild(gameType);
-    leftSection.appendChild(chipList);
+    leftSection.appendChild(chipsDiv);
     rightSection.appendChild(totalChips);
     rightSection.appendChild(playerCount);
     rightSection.appendChild(gameLength);
@@ -63,6 +56,44 @@ function createCard(cardData) {
     card.appendChild(infoBox);
     // Insérer la carte dans le document
     document.getElementById('cards-container').appendChild(card);
+}
+
+function chipToDiv(chipList) {
+    let chipListDiv = document.createElement('div');
+    chipListDiv.setAttribute('class', 'chip-list');
+    chipList.forEach(chip => {
+        let divChip = document.createElement('div');
+        divChip.setAttribute('class', 'chip-infos');
+
+        let imgChipIcon = document.createElement('img');
+        imgChipIcon.setAttribute('class', 'chip-icon');
+        imgChipIcon.setAttribute('src', colorToIconURL(chip.color));
+
+        let chipInfo = document.createElement('p');
+        chipInfo.textContent = ` ${chip.count} (${chip.value}$)`;
+
+        divChip.appendChild(imgChipIcon);
+        divChip.appendChild(chipInfo);
+
+        chipListDiv.appendChild(divChip);
+    });
+    return chipListDiv;
+}
+
+function colorToIconURL(color) {
+    let iconURL = "";
+    switch (color) {
+        case 'rouge': iconURL = "images/poker-chip icons red.png"; break;
+        case 'bleu': iconURL = "images/poker-chip icons blue.png"; break;
+        case 'vert': iconURL = "images/poker-chip icons green.png"; break;
+        case 'noir': iconURL = "images/poker-chip icons.png"; break;
+        case 'blanc': iconURL = "images/poker-chip icons white.png"; break;
+        case 'jaune': iconURL = "images/poker-chip icons yellow.png"; break;
+        case 'violet': iconURL = "images/poker-chip icons purple.png"; break;
+        case 'gris': iconURL = "images/poker-chip icons grey.png"; break;
+        case 'orange': iconURL = "images/poker-chip icons orange.png"; break;
+    }
+    return iconURL;
 }
 
 function insertCards(cardDataArray) {
